144d48ae0b81d046cc5a897009c25922e3751105
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2023-04-13 22:00:20 -0300

    first commit

---
+++ leitor_arquivo.php
@@ -0,0 +1,12 @@
+<?php
+
+//abre o arquivo
+$arquivo = fopen('./arquivo.txt', 'r');
+
+//enquanto não chegar no fim do arquivo ele continua a ler
+while(!feof($arquivo)) {
+    //avança até a proxima quebra de linha, mudando o cursor de linha
+    $cursor = fgets($arquivo);
+}
+
+fclose($arquivo);
